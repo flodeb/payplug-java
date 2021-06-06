aea971420930bc07ab6cd8c69718ab8d0fcf3642
Florian Debiesse <florian.debiesse@gmail.com>
2021-06-06 20:39:09 +0200

    (CHORE) Pass failure code to string

--- src/main/java/com/flodeb/payplug/model/Failure.java
+++ src/main/java/com/flodeb/payplug/model/Failure.java
@@ -1,13 +1,12 @@
 package com.flodeb.payplug.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.flodeb.payplug.model.enums.FailureCode;
 import lombok.Data;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 public class Failure {
-    private FailureCode code;
+    private String code;
 
     private String message;
 }
